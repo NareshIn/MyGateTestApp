92d9fe942902c85da0e912e8f0881320614bdc18
NareshPamoj <naresh@instachk.today>
2019-02-02 11:09:34 +0530

    Initial Commit

---
+++ MyGateTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyGateTestApp
+//
+//  Created by Naresh on 02/02/19.
+//  Copyright © 2019 Naresh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
